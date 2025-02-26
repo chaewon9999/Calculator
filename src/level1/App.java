7b24560c064a0d07cd1ef6958a535f46a5efced2
chaewon9999 <w0729@naver.com>
2025-02-26 13:51:23 +0900

    lv1 code

--- src/level1/App.java
+++ src/level1/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int result = 0;
 
         while (true) {
             /**첫 번째 숫자 입력**/
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = scanner.nextInt();
+            int firstNum = sc.nextInt();
             if (firstNum < 0) {
                 System.out.println("잘못된 숫자입니다.");
                 continue;
@@ -19,17 +19,17 @@ public class App {
 
             /**두 번째 숫자 입력**/
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = scanner.nextInt();
+            int secondNum = sc.nextInt();
             if (secondNum < 0) {
                 System.out.println("잘못된 숫자입니다.");
                 continue;
             }
 
-            scanner.nextLine(); //줄바꿈
+            sc.nextLine(); //줄바꿈
 
             /**연산기호 입력**/
             System.out.print("사칙 연산 기호를 입력하세요(+, -, *, /) : ");
-            char operator = scanner.next().charAt(0);
+            char operator = sc.next().charAt(0);
 
             /**연산 검증**/
             if (operator == '+') {
@@ -47,11 +47,11 @@ public class App {
 
             /**연산 결과 반환**/
             System.out.println("계산 결과: " + result);
-            scanner.nextLine();
+            sc.nextLine();
 
             /**exit를 입력 받으면 종료**/
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
-            String choice = scanner.nextLine();
+            String choice = sc.nextLine();
             if (choice.toLowerCase().equals("exit")) {
                 System.out.println("계산기를 종료합니다.");
                 return;
