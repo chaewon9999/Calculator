d7c3421d2db9794b15af4e9f6e1aab958a2f7c62
chaewon9999 <w0729@naver.com>
2025-02-26 19:50:35 +0900

    Lv2

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
     private List<Integer> resultList = new ArrayList<>();
     private int result;
 
+    /**연산하기**/
     public int calculate(int num1, int num2, char operate) {
         if (operate == '+') {
             result =  num1 + num2;
@@ -21,18 +22,22 @@ public class Calculator {
         return result;
     }
 
+    /**메세지와 함께 resultList의 마지막 값 반환**/
     public void getResult() {
         System.out.println("계산 결과: " + resultList.get(resultList.size() - 1));
     }
 
+    /**resultList에 값 넣어줌**/
     public void setResult(int num) {
         resultList.add(num);
     }
 
+    /**resultList 반환**/
     public List<Integer> getResultList() {
         return resultList;
     }
 
+    /**resultList의 첫 번째 값 삭제**/
     public void removeResult() {
         resultList.remove(0);
     }
